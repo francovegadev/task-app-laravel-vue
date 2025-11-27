7217307886802ec95c83df69499b61d28a903c89
vegafranco018 <fvega@quorumit.com>
2025-11-27 19:10:00 -0300

    add item model test

---
+++ backend/app/Models/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table='items';
+    protected $fillable = [
+        'name',
+        'description',
+        'stock',
+        'price',
+        'category',
+        'user_id'
+    ];
+}
